d9c9a3a96163a3e8220aa6ecd04d1a8ecf0d5864
StarXin <42883457+StarXin1@users.noreply.github.com>
2024-01-08 14:43:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ chat_msg = st.chat_message("user")
 chat_msg.write("这是一个聊天框~")
 
 container = chat_msg.container(border=True)
+container.markdown("这是一条markdown消息")
 container.write("这个容器在聊天框里面吗？")
 
 st.write("这个消息在聊天框外面吗？")
