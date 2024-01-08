e30c9d83cd8ad9c287f7d78acb58e2a854766f79
StarXin <42883457+StarXin1@users.noreply.github.com>
2024-01-08 14:45:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 chat_msg = st.chat_message("user")
 chat_msg.write("这是一个聊天框~")
-chat_msg.markdown("这是一条markdown消息1")
+chat_msg.markdown(''':red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in] :gray[pretty] :rainbow[colors].''')
 
 container = chat_msg.container(border=True)
-container.markdown("这是一条markdown消息2")
+container.markdown("Here's a bouquet &mdash; :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 container.write("这个容器在聊天框里面吗？")
 
 st.write("这个消息在聊天框外面吗？")
