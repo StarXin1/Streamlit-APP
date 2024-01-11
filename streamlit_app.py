3050581db8e62050c0f9938f483f128d2afc6163
StarXin <42883457+StarXin1@users.noreply.github.com>
2024-01-11 11:35:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-def change_btn_name(self):
-  self.label = "关闭语音"
-
 chat_msg = st.chat_message("user")
 chat_msg.write("这是一个聊天框~")
 chat_msg.markdown(''':red[Streamlit] :orange[可以] :green[生成] :blue[各种] :violet[颜色] :gray[的] :rainbow[文本].''')
@@ -13,5 +10,5 @@ container.write("这个容器在聊天框里面吗？")
 
 st.write("这个消息在聊天框外面吗？")
 
-if st.button("语音交流", on_click=change_btn_name):
+if st.button("语音交流"):
   st.write("按钮名字改变")
